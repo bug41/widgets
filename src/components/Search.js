e5f7bef8df5654df87656c232b33a03c4ae984dc
bug41 <bugkingus@gmail.com>
2022-03-13 01:29:08 +0900

    widgets

---
+++ src/components/Search.js
@@ -0,0 +1,31 @@
+import React, {useEffect, useState} from 'react';
+
+const Search = () => {
+
+    const [term, setTerm] = useState('');
+
+    console.log('I RUN WITH EVERY RENDER')
+
+    useEffect(() => {
+        //console.log('I ONLY RUN ONCE')
+        console.log('I RUN AFTER EVERY RUNDER AND AT INITIAL RENDER')
+    })
+
+    return (
+        <div>
+            <div className="ui form">
+                <div className="field">
+                    <label>Enter Search Term</label>
+                    <input 
+                        value={term}
+                        onChange={e => setTerm(e.target.value)}
+                        className="input"
+                    />
+                </div>
+            </div>
+        </div>
+    );
+
+}
+
+export default Search;
