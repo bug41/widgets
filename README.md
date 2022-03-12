0be0ec80c437fe4d9d695a56243848ec77d23882
bug41 <bugkingus@gmail.com>
2022-03-13 01:47:28 +0900

    backup

--- README.md
+++ README.md
@@ -3,3 +3,33 @@
 React.Fragment 등록 해서 border 겹치는 부분 없애기
 
 onClick={() => onTitleClick(index)} , () 안적을경우 일괄 자동 실행됨 
+
+
+
+### 방법 1
+
+useEffect(() => {
+    const search = async () => {
+        await axios.get('aaaa');
+    }
+    search();
+}, [term]);
+
+
+### 방법 2
+
+useEffect(() => {
+    (async () => {
+        await axios.get('aaaa');
+    })();        
+}, [term]);
+
+
+### 방법 3
+
+useEffect(() => {
+    axios.get('aaaa')
+    .then((response) => {
+        console.log(response.data);
+    });
+}, [term]);
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,15 +1,44 @@
 import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 
 const Search = () => {
 
     const [term, setTerm] = useState('');
+    const [results, setResults] = useState([]);
 
-    console.log('I RUN WITH EVERY RENDER')
+    useEffect(() => {        
+        const search = async () => {
+            const {data} = await axios.get('https://en.wikipedia.org/w/api.php', {
+                params: {
+                    action : 'query',
+                    list: 'search',
+                    origin: '*',
+                    format : 'json',
+                    srsearch: term
+                }
+            });
 
-    useEffect(() => {
-        //console.log('I ONLY RUN ONCE')
-        console.log('I RUN AFTER EVERY RUNDER AND AT INITIAL RENDER')
-    })
+            setResults(data.query.search);
+        }
+
+        if(term){
+            search();
+        }
+    }, [term]);
+
+
+    const renderedResults = results.map((result) => {
+        return(
+        <div className="item" key={result.pageid}>
+            <div className="content">
+                <div className="title">
+                    {result.title}
+                </div>
+                {result.snippet}                
+            </div>
+        </div>
+            )
+    });
 
     return (
         <div>
@@ -22,6 +51,9 @@ const Search = () => {
                         className="input"
                     />
                 </div>
+            </div>
+            <div className="ui celled list">
+                {renderedResults}
             </div>
         </div>
     );
