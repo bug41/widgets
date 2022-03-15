22305d40f02603080d1a27c583373500b3480643
bug41 <bugkingus@gmail.com>
2022-03-16 06:55:57 +0900

    search.js

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const Search = () => {
 
-    const [term, setTerm] = useState('');
+    const [term, setTerm] = useState('cars');
     const [results, setResults] = useState([]);
 
     useEffect(() => {
@@ -21,16 +21,20 @@ const Search = () => {
             setResults(data.query.search);  
         }
 
-        const timeoutId =  setTimeout(() => {            
-            if(term){
-                search();
+        if(term && !results.length) {
+            search();
+        }else{            
+            const timeoutId =  setTimeout(() => {            
+                if(term){
+                    search();
+                }
+            }, 1000)
+            
+            return () => {
+                clearTimeout(timeoutId);
             }
-        }, 500)
-
-        return () => {
-            clearTimeout(timeoutId);
         }
-
+            
     }, [term]);
 
 
